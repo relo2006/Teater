c143747dde699a833e6225432f895608eddb17f1
relo2006 <153818263+relo2006@users.noreply.github.com>
2023-12-14 11:16:14 +0100

    Update Teater.py

--- Teater.py
+++ Teater.py
@@ -1,1 +1,25 @@
+class Person:
+  def __init__(self, navn, adresse, epost, telefon, billetter = [], type = ):
+    self.navn = navn
+    self.adresse = adresse
+    self.epost = epost
+    self.telefon = telefon
+    self.billetter = billetter
+
+  def visBillett(self):
+    return self.billetter
+
+class Billett:
+  def __init__(self, forestilling, dag, sal, person):
+    self.forestilling = forestilling
+    self.dag = dag
+    self.sal = sal
+    self.person = person
+  def typeBillett(self):
+    if person
+  
+  def __str__(self):
+    return f'Forestilling: {self.forestilling} \nDag: {self.dag} \nSal: {self.sal} \nPerson: {self.person}'
+
+
 
