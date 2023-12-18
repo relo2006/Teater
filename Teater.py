4d4b4e2c035c85c99b835fb8fc76679d9a14c6c3
relo2006 <153818263+relo2006@users.noreply.github.com>
2023-12-18 12:57:39 +0100

    Update Teater.py

--- Teater.py
+++ Teater.py
@@ -15,11 +15,28 @@ class Billett:
     self.dag = dag
     self.sal = sal
     self.person = person
-  def typeBillett(self):
-    if person
   
   def __str__(self):
     return f'Forestilling: {self.forestilling} \nDag: {self.dag} \nSal: {self.sal} \nPerson: {self.person}'
+
+class Forestilling:
+  def __init__(self, navn, sal):
+    self.navn = navn
+    self.sal = sal
+
+  LedigePlasser = self.sal.plasser
+  
+  def visLedigePlasser(self):
+    return LedigePlasser
+    
+
+class Sal:
+  def __init__(self, navn, plasser):
+    self.navn = navn
+    self.plasser = plasser
+
+
+    
 
 
 
